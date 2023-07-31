2e2020c840ff5e0dfac8c8df776a2c237abd33d7
Lisha Yang <lishayang@Lishas-MacBook-Pro.local>
2023-07-31 08:30:06 +0200

    router-dom

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -5,7 +5,7 @@ const About = () => {
     <div className='bg-background'>
       <div className='flex flex-col justify-center mx-auto px-4 text-xl md:text-3xl md:px-44'>
 
-      <p>I've created this space to document my progress with <span className='bg-yellow-highlighter px-2'>webdev</span>. A space to post just about anything and everything. I'm mainly focusing on developing my skills in <span className='bg-yellow-highlighter px-2'>JS, React and Figma.</span></p>
+      <p>I've created this space to document my progress with <span className='bg-yellow-highlighter px-2'>webdev</span> and <span className='bg-yellow-highlighter px-2'>creative coding</span>. A space to post just about anything and everything. I'm mainly focusing on developing my skills in <span className='bg-yellow-highlighter px-2'>JS, React and P5js.</span></p>
       </div>
     </div>
   )
